5d859f08cc5a896b7024eafc07a6537e9156bcdf
unknown <adewaleadeboye10@gmail.com>
2023-05-23 06:36:32 +0100

    update

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Person from './components/profile/Person'
+
+function App() {
+
+  return (
+    <div className="App">
+      <Person fullName='Adewale Adeboye' bio=' I build scalable web applications.
+ ' profession='Software Developer'/>
+    </div>
+  )
+}
+
+export default App
